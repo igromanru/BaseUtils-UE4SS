9e9bc07208a20272a526df2240725395d2a5a639
igromanru <igromanru@yahoo.de>
2024-08-31 00:57:37 +0200

    feat: Add selfdeletion to the script

--- prepare_release.py
+++ prepare_release.py
@@ -1,6 +1,8 @@
 import os
+import sys
 import shutil
 import fnmatch
+import subprocess
 
 ITEMS_TO_REMOVE = ['.git*', '*.gitignore', '*.gitattributes', '*.gitmodules', '*.md', '*.md', '*.json', 'AFUtilsDebug.lua', 'nexusmods']
 
@@ -35,9 +37,20 @@ def remove_item(path):
         except OSError as e:
             print(f"Error removing directory {path}: {e}")
 
+def self_delete():
+    script_path = os.path.abspath(__file__)
+    if sys.platform.startswith('win'):
+        cmd = f'cmd /c ping localhost -n 2 > nul && del "{script_path}"'
+        subprocess.Popen(cmd, shell=True, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
+    else:
+        os.system(f'rm "{script_path}"')
+
 if __name__ == "__main__":
     current_directory = os.getcwd()
     print(f"Starting cleanup in: {current_directory}")
     print(f"Items to be removed: {ITEMS_TO_REMOVE}")
     remove_specified_files(current_directory)
     print("Cleanup completed.")
+
+    print("Deleting the script itself...")
+    self_delete()
