6a00e32f8a09d6080b846700a91c278d4973e4ff
igromanru <igromanru@yahoo.de>
2024-09-02 19:01:04 +0200

    feat: Add LogDebug.lua with LogCharacterMovementComponent and LogMovementComponent functions

---
+++ LogDebug.lua
@@ -0,0 +1,39 @@
+
+local currentDir = debug.getinfo(1, "S").source:match("@(.+\\)LogDebug.lua")
+if currentDir then
+    package.path = package.path .. ';' .. currentDir .. '?.lua'
+    require("BaseUtils")
+end
+
+---@param MovementComponent UMovementComponent
+---@param Prefix string?
+function LogMovementComponent(MovementComponent, Prefix)
+    if not MovementComponent or not MovementComponent:IsValid() then return end
+    Prefix = Prefix or ""
+
+    local velocity = VectorToUserdata(MovementComponent.Velocity)
+    LogDebug(Prefix .. "Current Speed: ", GetKismetMathLibrary():VSize(velocity))
+    LogDebug(Prefix .. "Velocity: " .. VectorToString(velocity))
+    LogDebug(Prefix .. "GetMaxSpeed: ", MovementComponent:GetMaxSpeed())
+    LogDebug(Prefix .. "GetGravityZ: ", MovementComponent:GetGravityZ())
+end
+
+---@param MovementComponent UCharacterMovementComponent
+---@param Prefix string?
+function LogCharacterMovementComponent(MovementComponent, Prefix)
+    if not MovementComponent or not MovementComponent:IsValid() then return end
+    Prefix = Prefix or ""
+
+    LogDebug(Prefix .. "GravityScale: ", MovementComponent.GravityScale)
+    -- LogDebug(Prefix .. "MaxStepHeight: ", MovementComponent.MaxStepHeight)
+    LogDebug(Prefix .. "JumpZVelocity: ", MovementComponent.JumpZVelocity)
+    LogDebug(Prefix .. "GravityDirection: " .. VectorToString(MovementComponent.GravityDirection))
+    LogDebug(Prefix .. "MovementMode (enum 0-6): ", MovementComponent.MovementMode)
+    LogDebug(Prefix .. "MaxWalkSpeed: ", MovementComponent.MaxWalkSpeed)
+    LogDebug(Prefix .. "MaxWalkSpeedCrouched: ", MovementComponent.MaxWalkSpeedCrouched)
+    LogDebug(Prefix .. "MaxSwimSpeed: ", MovementComponent.MaxSwimSpeed)
+    LogDebug(Prefix .. "MaxFlySpeed: ", MovementComponent.MaxFlySpeed)
+    LogDebug(Prefix .. "MaxCustomMovementSpeed: ", MovementComponent.MaxCustomMovementSpeed)
+    LogDebug(Prefix .. "MaxAcceleration: ", MovementComponent.MaxAcceleration)
+    LogMovementComponent(MovementComponent, Prefix)
+end
